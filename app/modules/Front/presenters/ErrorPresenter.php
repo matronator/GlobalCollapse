8512579189889a772cee79ea2716f5e5d2373be7
Matej Bystricky <info@matronator.com>
2020-06-12 20:50:44 +0200

    fixes

--- app/modules/Front/presenters/ErrorPresenter.php
+++ app/modules/Front/presenters/ErrorPresenter.php
@@ -36,12 +36,11 @@ class ErrorPresenter implements Nette\Application\IPresenter
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
 
-
 		// 500
 		$this->logger->log($exception, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
+				require __DIR__ . '/../templates/Error/500.phtml';
 			}
 		});
 	}
