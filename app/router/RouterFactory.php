b1b29b9281cc855d37533e345bb1211508434344
Matronator <5470780+matronator@users.noreply.github.com>
2021-06-16 23:47:12 +0200

    update router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -16,36 +16,36 @@ class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router[] = new Route('index.php', 'Front:Default:default', Route::ONE_WAY);
+		$router->addRoute('index.php', 'Front:Default:default', Route::ONE_WAY);
 
 		/////////////////////// ADMIN ROUTES ///////////////////////
-		$router[] = $adminRouter = new RouteList('Admin');
-		$adminRouter[] = new Route('admin/<presenter>/<action>[/<id>]', 'Default:default'); //most general route
+		$router->withModule('Admin')
+			->addRoute('admin/<presenter>/<action>[/<id>]', 'Default:default'); //most general route
 
 		/////////////////////// FRONT ROUTES ///////////////////////
-		$router[] = $frontRouter = new RouteList('Front');
-		$frontRouter[] = new Route('sitemap.xml', 'Sitemap:default');
-		$frontRouter[] = new Route('sitemap', 'Sitemap:default');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]news[/<page>[/<id>]]', 'Info:news');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]news/<post>/<id>', 'Info:post');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]copyright-notice', 'Info:copyright');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]terms-of-use', 'Info:tos');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]privacy-policy', 'Info:privacy');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]rest', 'Default:rest');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]training', 'Default:training');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]darknet', 'City:darknet');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]wastelands', 'City:wastelands');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]lands', 'Buildings:lands');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]buildings', 'Buildings:default');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]player/detail/<user>', 'Player:detail');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]assaults', 'Assaults:default');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]assaults/detail/<user>', 'Assaults:detail');
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]assaults/assault/<match>', 'Assaults:assault');
+		$router->withModule('Front')
+			->addRoute('sitemap.xml', 'Sitemap:default')
+			->addRoute('sitemap', 'Sitemap:default')
+			->addRoute('[<locale=en en|ru|cs>/]news[/<page>[/<id>]]', 'Info:news')
+			->addRoute('[<locale=en en|ru|cs>/]news/<post>/<id>', 'Info:post')
+			->addRoute('[<locale=en en|ru|cs>/]copyright-notice', 'Info:copyright')
+			->addRoute('[<locale=en en|ru|cs>/]terms-of-use', 'Info:tos')
+			->addRoute('[<locale=en en|ru|cs>/]privacy-policy', 'Info:privacy')
+			->addRoute('[<locale=en en|ru|cs>/]rest', 'Default:rest')
+			->addRoute('[<locale=en en|ru|cs>/]training', 'Default:training')
+			->addRoute('[<locale=en en|ru|cs>/]darknet', 'City:darknet')
+			->addRoute('[<locale=en en|ru|cs>/]wastelands', 'City:wastelands')
+			->addRoute('[<locale=en en|ru|cs>/]lands', 'Buildings:lands')
+			->addRoute('[<locale=en en|ru|cs>/]buildings', 'Buildings:default')
+			->addRoute('[<locale=en en|ru|cs>/]player/detail/<user>', 'Player:detail')
+			->addRoute('[<locale=en en|ru|cs>/]assaults', 'Assaults:default')
+			->addRoute('[<locale=en en|ru|cs>/]assaults/detail/<user>', 'Assaults:detail')
+			->addRoute('[<locale=en en|ru|cs>/]assaults/assault/<match>', 'Assaults:assault')
 
 		// APIs
-		$frontRouter[] = new Route('api/job', 'Api:job');
+		->addRoute('api/job', 'Api:job')
 
-		$frontRouter[] = new Route('[<locale=en en|ru|cs>/]<presenter>/<action>[/<id>]', 'Default:default'); //most general route
+		->addRoute('[<locale=en en|ru|cs>/]<presenter>/<action>[/<id>]', 'Default:default'); //most general route
 
 		return $router;
 	}
